6f959502fd48d5d3f4118c607789642d85a0d072
anasteisha26 <kaliutchykanastasiya@gmail.com>
2024-06-21 16:14:59 +0300

    Add files via upload

---
+++ 2ndQuery.sql
@@ -0,0 +1,26 @@
+/* This script is presented in two queries. The first one creates a view with a calculated column 'age'. 
+The second query calculates the average age of people in a room and returns 5 rooms with the lowest average age. 
+*/
+
+CREATE VIEW Age_of_Students AS
+SELECT
+id,
+name,
+EXTRACT(YEAR FROM DATE(NOW())) - EXTRACT(YEAR FROM DATE(birthday)) as age
+FROM students;
+
+
+SELECT
+r.id,
+r.name,
+COUNT(s.name) as amount_of_students_living_in_a_room,
+AVG(aos.age) as average_age
+FROM rooms as r
+LEFT JOIN students as s
+ON r.id = s.room 
+LEFT JOIN age_of_students as aos
+ON aos.id = s.id
+GROUP BY r.id, r.name
+ORDER BY average_age ASC
+LIMIT 5
+;
