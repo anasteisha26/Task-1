c640ad43dae5389278033045671362edb80cb735
anasteisha26 <kaliutchykanastasiya@gmail.com>
2024-06-21 15:58:13 +0300

    Add files via upload

---
+++ 1stquery.sql
@@ -0,0 +1,17 @@
+CREATE VIEW aamount_of_students AS 
+SELECT  
+r.id, 
+r.name as room_n, 
+s.name FROM rooms as r 
+LEFT JOIN students as s 
+ON s.room = r.id
+;
+
+SELECT
+id,
+room_n as 'name of the room',
+COUNT(name) as 'amount of people living in a room'
+FROM students_rooms.amount_of_students
+GROUP BY id, room_n
+ORDER BY id
+;
