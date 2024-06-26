2c2131980654b0148de7b3a374148f88ae0a45ef
anasteisha26 <kaliutchykanastasiya@gmail.com>
2024-06-26 13:59:02 +0300

    Update 3rdQuery.sql

--- 3rdQuery.sql
+++ 3rdQuery.sql
@@ -1,19 +1,13 @@
-/* 
-This script shows the youngest, the oldest people in the room, and their age difference
-*/
 SELECT
 r.id,
 r.name,
-AVG(aos.age),
-MAX(aos.age) as the_oldest_in_a_room,
-MIN(aos.age) as the_youngest_in_a_room,
-MAX(aos.age) - MIN(aos.age) as difference_between_ages
+AVG(EXTRACT(YEAR FROM DATE(NOW())) - EXTRACT(YEAR FROM DATE(birthday))) as average_age_in_a_room ,
+MAX(EXTRACT(YEAR FROM DATE(NOW())) - EXTRACT(YEAR FROM DATE(birthday))) as the_oldest_in_a_room,
+MIN(EXTRACT(YEAR FROM DATE(NOW())) - EXTRACT(YEAR FROM DATE(birthday))) as the_youngest_in_a_room,
+MAX(EXTRACT(YEAR FROM DATE(NOW())) - EXTRACT(YEAR FROM DATE(birthday))) - MIN(EXTRACT(YEAR FROM DATE(NOW())) - EXTRACT(YEAR FROM DATE(birthday))) as difference_between_ages
 FROM rooms as r
 INNER JOIN students as s
 ON r.id = s.room
-LEFT JOIN age_of_students as aos
-ON aos.id = s.id
 GROUP BY r.id, r.name
 ORDER BY difference_between_ages DESC
-LIMIT 5
-;
+LIMIT 5;
